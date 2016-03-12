48fee1913809fd85f680e7e4a5d131002dc2e862
andrey <gentoo1993@gmail.com>
2016-03-12 18:58:56 +0300

    oops

--- src/main/java/bus/service/db/DB.java
+++ src/main/java/bus/service/db/DB.java
@@ -13,7 +13,7 @@ public class DB {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://localhost:3306/bus_service?useSSL=false");
         config.setUsername("root");
-        config.setPassword("47916834");
+        config.setPassword("root");
         config.setDriverClassName("com.mysql.jdbc.Driver");
 
         dataSource = new HikariDataSource(config);
